a8e590e4776973bae9c284a421421568b7e347ba
igithubers <69648238+igithubers@users.noreply.github.com>
2025-08-08 14:50:03 +0300

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,12 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App'
-import { Toaster } from 'sonner'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <Toaster position="top-center" />
-  </React.StrictMode>,
-)
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
